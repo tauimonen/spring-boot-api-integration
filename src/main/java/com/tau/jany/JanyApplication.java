6d34eaf7db734747ea3761711fba388de9942301
tauimonen <tauimonen@gmail.com>
2025-01-23 20:13:59 +0200

    first commit

---
+++ src/main/java/com/tau/jany/JanyApplication.java
@@ -0,0 +1,13 @@
+package com.tau.jany;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JanyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JanyApplication.class, args);
+	}
+
+}
