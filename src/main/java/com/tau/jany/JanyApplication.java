33479a134353cf518a88ad08141e53b53a70c483
tauimonen <tauimonen@gmail.com>
2025-01-24 10:53:45 +0200

    Create a commandLineRunner and set defautHeader

--- src/main/java/com/tau/jany/JanyApplication.java
+++ src/main/java/com/tau/jany/JanyApplication.java
@@ -1,13 +1,36 @@
 package com.tau.jany;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.client.RestClient;
 
 @SpringBootApplication
 public class JanyApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(JanyApplication.class, args);
+	}
+
+	@Bean
+	CommandLineRunner commandLineRunner(RestClient.Builder builder) {
+		return args -> {
+			var client = builder
+					.baseUrl("http://localhost:8080")
+					.defaultHeader(HttpHeaders.ACCEPT, MediaType.APPLICATION_JSON_VALUE)
+					.build();
+
+			ResponseEntity<GroupedDaily> entity = client.get()
+					.uri("/api/jany")
+					.retrieve()
+					.toEntity(GroupedDaily.class);
+
+			System.out.println(entity);
+		};
 	}
 
 }
